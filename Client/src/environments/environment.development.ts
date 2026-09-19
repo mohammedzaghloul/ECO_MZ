d656e568f7254fead173d5f3a543247ec89f351a
mohammedzaghloul <145876544+mohammedzaghloul@users.noreply.github.com>
2026-09-19 17:34:08 +0300

    chore client: update workspace config, dependencies, proxy and PWA setup

---
+++ Client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+    basurl:'/api/',
+    authApiUrl: 'http://localhost:5045/api/',
+    stripePublishableKey: 'pk_test_51TZblGEPkqEOXJpU7lsO1FIL8WdCW5hu20gPT51dvQHAIm63JO7vKeET6xdpoUyH1jjPfi3ZdJngG7V7UkdZ1EbF00y0N48tu8'
+};
---
+++ Client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  basurl: '/api/',
+  authApiUrl: '/api/'
+};
